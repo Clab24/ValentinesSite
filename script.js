f73021b97968427993a0ea97931f67ce06813792
Clab24 <158280458+Clab24@users.noreply.github.com>
2024-01-31 12:30:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "SIUM, ci vediamo il 14 Baby😎😘😘";
+    yesMessage.textContent = "Ottimo, ci vediamo il 14 Baby😎😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
